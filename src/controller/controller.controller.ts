7eb3dc2a6f2e2b5ca3dfbccf3027d336ad91744b
Cleomir <cleomir.mg@hotmail.com>
2020-12-02 18:35:38 +0200

    Add PUT and DELETE

--- src/controller/controller.controller.ts
+++ src/controller/controller.controller.ts
@@ -1,22 +1,44 @@
-import { Controller, Get, Header, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateDto } from './controller.dto';
 
 @Controller('controller')
 export class ControllerController {
-  @HttpCode(204)
+  //   @HttpCode(204)
   @Post()
   @Header('Cache-Control', 'none')
-  create(): string {
-    return 'created';
+  create(@Body() createDto: CreateDto): string {
+    console.log(createDto.name);
+    return createDto.name;
   }
 
   @Get()
-  findAll(): string {
+  async findAll(): Promise<string> {
     return 'Hello controller';
   }
 
   @Get(':id')
   findOne(@Param() params): string {
     console.log(params.id);
-    return `Hello id : ${params.id}`;
+    return `Get id : ${params.id}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `Update id ${id}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `Delete id ${id}`;
   }
 }
