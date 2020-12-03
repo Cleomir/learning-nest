55afbc992ef3e10a1d379f7ce8ea9ec6113add44
Cleomir <cleomir.mg@hotmail.com>
2020-12-03 09:00:06 +0200

    Accesss exported module

--- src/service/service.service.ts
+++ src/service/service.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { ExportedService } from 'src/module/exported.service';
 
 @Injectable()
-export class ServiceService {
+export class ServiceService implements OnModuleInit {
+  private service: ExportedService;
+  constructor(private moduleRef: ModuleRef) {}
+  onModuleInit() {
+    this.service = this.moduleRef.get(ExportedService, { strict: false });
+  }
+
   private readonly names: string[] = [];
 
   create(name: string) {
+    this.service.printName('John doe');
     this.names.push(name);
   }
 
